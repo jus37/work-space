6454b6e4cc083785d006b7d02553d2f40c8f8c71
jus37 <jus_37@outlook.jp>
2020-11-25 16:48:48 +0900

    ユーザーモデル
    バリデーション
    アクティブストレージとのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :image
+  
+  with_options presence: true do
+    validates :name
+    validates :telephone
+    validates :image
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, presence: true, format: { with: PASSWORD_REGEX }
+
+
+
 end
