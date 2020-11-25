6cd7ef6d6856ca757db8ba4df2faa84797970296
jus37 <jus_37@outlook.jp>
2020-11-25 16:36:28 +0900

    ユーザーテーブルの編集

--- db/migrate/20201125065734_devise_create_users.rb
+++ db/migrate/20201125065734_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+      t.string :telephone, null: false
+      t.text :comment
+      
+
 
       ## Recoverable
       t.string   :reset_password_token
