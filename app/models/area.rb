f6102aa4adec78e2298f8cc3558832b99951a3ab
jus37 <jus_37@outlook.jp>
2020-11-26 18:15:15 +0900

    activehashテーブル作成

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,5 +1,20 @@
 class Area < ActiveHash::Base
 
+  self.data = [
+    { id: 1, name: '--' },  
+    { id: 2, name: '梅田' },
+    { id: 3, name: '難波' },
+    { id: 4, name: '天王寺' },
+    { id: 5, name: '本町' },
+    { id: 6, name: '新大阪' },
+    { id: 7, name: '北浜' },
+    { id: 8, name: '江坂' },
+    { id: 9, name: '京橋' },
+    { id: 10, name: '堺' },
+    { id: 11, name: '高槻' },
+  ]
+
+
   include ActiveHash::Associations
   has_many :shops
 end
--- app/models/characteristic.rb
+++ app/models/characteristic.rb
@@ -1,4 +1,15 @@
 class Characteristic < ActiveHash::Base
+
+  { id: 1, name: '--' },  
+  { id: 2, name: '安い' },
+  { id: 3, name: '無料wifiあり' },
+  { id: 4, name: '電源あり' },
+  { id: 5, name: '夜遅くまで営業' },
+  { id: 6, name: '個室あり' },
+  { id: 7, name: '雰囲気が良い' },
+  { id: 8, name: '静か' },
+]
+
   include ActiveHash::Associations
   has_many :shops
 
