50df923d7c50abf16385a4b338188569c62667cf
jus37 <jus_37@outlook.jp>
2020-11-26 17:12:35 +0900

    アクティブハッシュのバリデーション

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,4 +5,10 @@ class Shop < ApplicationRecord
   belongs_to :genre
   belongs_to :area
   belongs_to :characteristic
+
+  with_options numericality: { other_than: 1 } do
+    validates :genre
+    validates :area
+    validates :characteristic
+  end
 end
