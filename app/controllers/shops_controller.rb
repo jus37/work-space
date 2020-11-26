62ac8f791f456efa7260b248c5ee019adf520fe6
jus37 <jus_37@outlook.jp>
2020-11-26 14:37:14 +0900

    rubocop実行

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,4 @@
 class ShopsController < ApplicationController
-
   def index
   end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
-  
+
   with_options presence: true do
     validates :name
   end
@@ -13,10 +13,7 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[0-9]+\z/ }, length: { maximum: 11 } do
     validates :telephone
   end
-  
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, presence: true, format: { with: PASSWORD_REGEX }
-
-
-
 end
