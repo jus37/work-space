7d12f58f2c87b337baf6c40299542a3197934924
jus37 <jus_37@outlook.jp>
2020-11-26 18:25:48 +0900

    ルーティング、コントローラの実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,24 @@
 class ShopsController < ApplicationController
   def index
   end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :title, :content, :open_hour, :regular_holiday, :telephone, :address, :nearest_station, :capacity, :private_room, :characteristic_id, :genre_id, :area_id, :image)
+  end
+
 end
