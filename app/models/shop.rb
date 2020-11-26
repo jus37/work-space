6a1f7e51a6f3ee233a24efde557e70e57fc629a7
jus37 <jus_37@outlook.jp>
2020-11-26 18:18:06 +0900

    バリデーション設定

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,6 +6,15 @@ class Shop < ApplicationRecord
   belongs_to :area
   belongs_to :characteristic
 
+  with_options presence: true do
+    validates :name
+    validates :title
+    validates :open_hour
+    validates :regular_holiday
+    validates :address
+    validates :image
+  end
+  
   with_options numericality: { other_than: 1 } do
     validates :genre
     validates :area
