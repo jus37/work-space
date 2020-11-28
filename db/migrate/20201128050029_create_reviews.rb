321e8b2aa1a89cd0979a4436571f172c0e3c4053
jus37 <jus_37@outlook.jp>
2020-11-28 14:17:47 +0900

    マイグレーションファイル

--- db/migrate/20201128050029_create_reviews.rb
+++ db/migrate/20201128050029_create_reviews.rb
@@ -3,6 +3,12 @@ class CreateReviews < ActiveRecord::Migration[6.0]
     create_table :reviews do |t|
 
       t.timestamps
+
+      t.integer :review_point, null: false
+      t.date :date, null: false
+      t.string :content, null: false
+      t.references :user, null: false, foreign_keys: true
+      t.references :shop, null: false, foreign_keys: true
     end
   end
 end
