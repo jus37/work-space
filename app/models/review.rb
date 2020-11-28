ee2d669bdbeca64fb5418c7de1488ce29207ec86
jus37 <jus_37@outlook.jp>
2020-11-28 14:20:38 +0900

    バリデーションとアソシエーション

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :shop
+
+  with_options presence: true do
+    validates :review_point
+    validates :date
+    validates :content
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_085100) do
+ActiveRecord::Schema.define(version: 2020_11_28_050029) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2020_11_25_085100) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "review_point", null: false
+    t.date "date", null: false
+    t.string "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "shop_id", null: false
+    t.index ["shop_id"], name: "index_reviews_on_shop_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
