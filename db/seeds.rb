9abbdc3db2c047bbc9543a8ac46fabddd3e5b736
jus37 <jus_37@outlook.jp>
2020-12-02 11:34:10 +0900

    ジャンル、エリア、特徴のseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ジャンル登録
+Genre.create(name:"--")
+Genre.create(name:"カフェ")
+Genre.create(name:"コワーキングスペース")
+Genre.create(name:"カラオケボックス")
+Genre.create(name:"漫画喫茶")
+Genre.create(name:"ファストフード店")
+Genre.create(name:"図書館")
+Genre.create(name:"その他")
+
+# エリア登録
+Area.create(name:"--")
+Area.create(name:"梅田")
+Area.create(name:"難波")
+Area.create(name:"天王寺")
+Area.create(name:"新大阪")
+Area.create(name:"本町")
+Area.create(name:"堺")
+Area.create(name:"高槻")
+Area.create(name:"京橋")
+Area.create(name:"江坂")
+Area.create(name:"北浜")
+
+# おすすめ登録
+Characteristic.create(name:"安い")
+Characteristic.create(name:"夜遅くまで営業")
+Characteristic.create(name:"早朝から営業")
+Characteristic.create(name:"静か")
+Characteristic.create(name:"席が広い")
+Characteristic.create(name:"雰囲気が良い")
+Characteristic.create(name:"カード可")
+Characteristic.create(name:"電子マネー可")
+Characteristic.create(name:"禁煙席あり")
+
+# 店舗登録
+# Shop.create()
