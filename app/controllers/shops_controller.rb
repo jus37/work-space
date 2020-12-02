bb986243559603d5d1047f8dbc74c811dcfac243
jus37 <jus_37@outlook.jp>
2020-12-02 18:16:10 +0900

    トップジャンル検索項目の実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,8 +2,11 @@ class ShopsController < ApplicationController
 
   def index
     @shops = Shop.all
+    @top_genres = Genre.limit(5)
     @genres = Genre.all
+    @top_areas = Area.limit(5)
     @areas = Area.all
+    @top_characteristics = Characteristic.limit(5)
     @characteristics = Characteristic.all
   end
 
