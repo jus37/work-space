9232e17e89d7f8347dc1d52840bff627525585e8
jus37 <jus_37@outlook.jp>
2020-12-02 16:31:27 +0900

    不要な中間テーブルの削除

--- app/models/shop_area.rb
+++
@@ -1,4 +0,0 @@
-class ShopArea < ApplicationRecord
-  belongs_to :shop
-  belongs_to :area
-end
--- test/models/shop_area_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ShopAreaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
