68b87de34bfa53bb860e3e77d4728cb72c144d44
jus37 <jus_37@outlook.jp>
2020-12-03 12:32:06 +0900

    検索結果名の出現

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,2 +1,15 @@
 module ShopsHelper
+  def keyword
+    if @area.present?
+      "#{@area}による検索結果 #{@search_shops.count}件"
+    elsif @genre.present?
+      "#{@genre}による検索結果 #{@search_shops.count}件"
+    elsif @characteristic.present?
+      "#{@characteristic}による検索結果 #{@search_shops.count}件"
+    elsif @area_form.present?
+      "#{@area_form}による検索結果 #{@search_shops.count}件"
+    elsif @name_form.present?
+      "#{@name_form}による検索結果 #{@search_shops.count}件"
+    end
+  end
 end
