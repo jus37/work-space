93f760f0ce9ed13221e3f1b6330cf82b78cb4f9c
jus37 <jus_37@outlook.jp>
2020-12-05 13:26:55 +0900

    マイグレーションファイルの実装

--- db/migrate/20201205042255_create_histories.rb
+++ db/migrate/20201205042255_create_histories.rb
@@ -3,6 +3,8 @@ class CreateHistories < ActiveRecord::Migration[6.0]
     create_table :histories do |t|
 
       t.timestamps
+      t.references :user, null: false, foreign_keys: true
+      t.references :shop, null: false, foreign_keys: true
     end
   end
 end
