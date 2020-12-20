de2418b1e57968d367fb4b040950f101c39abe7c
jus37 <jus_37@outlook.jp>
2020-12-20 19:53:41 +0900

    ジャンルから探すCSS完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,8 +40,8 @@ Characteristic.create!(name:"禁煙席あり")
 
 # 店舗登録
 # Shop.create(name:"", title:"", content:"", open_hour:"", regular_holiday:"", telephone:"", address:"", nearest_station:"", capacity:"", private_room:"", wifi:"", power_supply:"", genre_id:"", area_id:"")
-Shop.create!(name:"スターバックスコーヒー NU茶屋町プラス店", title:"NU茶屋町にあるスタバ", content:"美味しい", open_hour:"9:00~22:00", regular_holiday:"日曜日", telephone:"0120444444", address:"大阪市北区", nearest_station:"阪急大阪梅田駅", capacity:"44席", private_room:"無", wifi:"有", power_supply:"有", genre_id:"2", area_id:"2")
-Shop.create!(name:"wework LINKS UMEDA", title:"駅直結梅田のコワーキングスペース", content:"最高", open_hour:"9:00~18:00", regular_holiday:"土日", telephone:"0120117117", address:"〒530-0011 大阪府大阪市北区大深町１−１", nearest_station:"JR大阪駅", capacity:"150席", private_room:"有", wifi:"有", power_supply:"有", genre_id:"3", area_id:"2")
+Shop.create!(name:"スターバックスコーヒー NU茶屋町プラス店", title:"NU茶屋町にあるスタバ", content:"美味しい", open_hour:"9:00~22:00", regular_holiday:"日曜日", telephone:"0120444444", address:"大阪市北区", nearest_station:"阪急大阪梅田駅", capacity:"44席", private_room:"無", wifi:"有", power_supply:"有", genre_id:"1", area_id:"1")
+Shop.create!(name:"wework LINKS UMEDA", title:"駅直結梅田のコワーキングスペース", content:"最高", open_hour:"9:00~18:00", regular_holiday:"土日", telephone:"0120117117", address:"〒530-0011 大阪府大阪市北区大深町１−１", nearest_station:"JR大阪駅", capacity:"150席", private_room:"有", wifi:"有", power_supply:"有", genre_id:"2", area_id:"1")
 
 #ユーザー登録
 User.create!(name: 'ジャス', email: 'test037@com', password: '111111a', password_confirmation: '111111a', telephone: "0120444444",comment: "hello!")
