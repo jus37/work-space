4a54c21c0c5320872ab176fd03fb7c69556a322a
jus37 <jus_37@outlook.jp>
2021-01-02 16:13:04 +0900

    jQueryを管理下に設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+// jQueryを管理下に設定
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
+  })
+)
 
 module.exports = environment
