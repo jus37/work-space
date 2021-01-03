a4c898cf12108ec0b4e31f44dd37d2f7fb8477a9
jus37 <jus_37@outlook.jp>
2021-01-03 19:25:02 +0900

    shop new CSS実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('./preview_shop')
 require('./preview_user')
 // = require jquery3
 // = require popper 
