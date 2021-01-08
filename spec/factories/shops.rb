d8935362587fbf693b6c64cefaeb032bdf91e82e
jus37 <jus_37@outlook.jp>
2021-01-08 15:32:11 +0900

    shop単体テスト完了

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,29 +1,28 @@
 FactoryBot.define do
   factory :shop do
-    name {Faker::Name}
-    title {Faker::String.random}
-    content {Faker::String.random}
-    open_hour {Faker::String.random}
-    regular_holiday {Faker::String.random}
+    name {Faker::Name.name}
+    title {Faker::Lorem.sentence}
+    content {Faker::Lorem.sentence}
+    open_hour {Faker::Lorem.word}
+    regular_holiday {Faker::Lorem.word}
     telephone {Faker::Number.number(digits: 11)}
     address {Faker::Address.full_address}
-    nearest_station {Faker::String.random}
-    capacity {Faker::String.random}
-    private_room {Faker::String.random}
-    wifi {Faker::String.random}
-    power_supply {Faker::String.random}
+    nearest_station {Faker::Lorem.word}
+    capacity {Faker::Lorem.word}
+    private_room {Faker::Lorem.word}
+    wifi {Faker::Lorem.word}
+    power_supply {Faker::Lorem.word}
+
+    association :genre
+    association :area
     genre_id {Faker::Number.within(range: 1..7)}
     area_id {Faker::Number.within(range: 1..10)}
+
 
     after(:build) do |item|
       item.images.attach(io: File.open('public/images/shop_test1.jpg'), filename: 'shop_test1.jpg')
       item.images.attach(io: File.open('public/images/shop_test2.jpg'), filename: 'shop_test2.jpg')
       item.images.attach(io: File.open('public/images/shop_test3.jpg'), filename: 'shop_test3.jpg')
     end
-  end
-
-  factory :shop_caracteristic do
-    association :shop
-    association :characteristic
   end
 end
