dc044e594aff193791b8a1b44025c61456a49c87
jus37 <jus_37@outlook.jp>
2021-01-08 11:10:04 +0900

    評価点のバリデーション修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,10 @@ class Review < ApplicationRecord
   belongs_to :shop
 
   with_options presence: true do
-    validates :review_point
     validates :date
     validates :title
+  end
+  with_options presence: true, format: { with: /[1-5]/ } do
+    validates :review_point
   end
 end
