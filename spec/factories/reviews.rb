2c4b707016d7ddbb76a2cfe417231ab51852e98f
jus37 <jus_37@outlook.jp>
2021-01-08 12:40:35 +0900

    factorybotの作成

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,10 @@
+Factories.define do
+  factory :review do
+    review_point {Faker::Number.within(range: 0..5)}
+    date {Faker::Date.between(to: Date.today)}
+    title {Faker::String.random}
+    comment {Faker::String.random}
+    association :user
+    association :shop
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+Factories.define do
+  factory :user do
+    name {Faker::Name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6, mix_case: true)}
+    password_confirmation {password}
+    telephone {Faker::Number.number(digits: 11)}
+    comment {Faker::Coffee.blend_name}
+    admin {1}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/user_test.jpg'), filename: 'user_test.jpg')
+    end
+  end
+end
