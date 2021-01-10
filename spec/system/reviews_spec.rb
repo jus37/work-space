4a475feec59bebba0f06269e9f535505fe824d1f
jus37 <jus_37@outlook.jp>
2021-01-10 17:43:08 +0900

    review結合テスト　異常系

--- spec/system/reviews_spec.rb
+++ spec/system/reviews_spec.rb
@@ -41,12 +41,38 @@ RSpec.describe "レビュー投稿", type: :system do
   end
   context 'レビューが投稿できないとき' do
     it '誤った情報ではレビュー投稿ができずに投稿ページへ戻ってくる' do
+      # ログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      find('input[class="sign-btn"]').click
       # 適当な店舗の詳細画面に移動する
+      visit shop_path(@shop.id)
       # 詳細画面に「行った」ボタンがあることを確認する
+      expect(page).to have_content('行った')
       # レビュー投稿画面へ移動する
+      visit new_shop_review_path(@shop.id)
       # レビュー情報を入力する
+      # find('#star').find("img[alt='5']").click
+      fill_in 'review_title', with: ""
+      fill_in 'review_comment', with: ""
+      fill_in 'review_date', with: ""
       # 送信ボタンを押してもレビューモデルのカウントが上がらないことを確認する
+      expect{
+        find('input[class="review-btn"]').click
+      }.to change {Review.count}.by(0)
       # レビュー投稿画面へ戻っていることを確認する
+      expect(current_path).to eq(shop_reviews_path(@shop.id))
+    end
+    it 'ログインせずに投稿しようとしてログイン画面に戻ってくる' do
+      # 適当な店舗の詳細画面に移動する
+      visit shop_path(@shop.id)
+      # 詳細画面に「行った」ボタンがあることを確認する
+      expect(page).to have_content('行った')
+      # レビュー投稿画面へ移動する
+      visit new_shop_review_path(@shop.id)
+      # ログイン画面に遷移していることを確認する
+      expect(current_path).to eq(new_user_session_path)
     end
   end
 end
