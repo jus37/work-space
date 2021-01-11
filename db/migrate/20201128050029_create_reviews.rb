dda946aeb55cb90e1ee66c2b22edf48c57cafcc0
jus37 <jus_37@outlook.jp>
2021-01-11 11:32:24 +0900

    clip historyの表示の修正

--- db/migrate/20201128050029_create_reviews.rb
+++ db/migrate/20201128050029_create_reviews.rb
@@ -7,7 +7,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.float :review_point, null: false
       t.date :date, null: false
       t.string :title, null: false
-      t.string :comment, null: false
+      t.string :comment
       t.references :user, null: false, foreign_key: true
       t.references :shop, null: false, foreign_key: true
     end
