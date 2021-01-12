a973b47ac1bfdb7c8f41dbdc3dff89561a2b6750
jus37 <jus_37@outlook.jp>
2021-01-12 16:56:18 +0900

    コントローラに記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+  
 end
