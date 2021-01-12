5512ed94a65f4de54bf9ff63b56e1a178cd72015
jus37 <jus_37@outlook.jp>
2021-01-12 17:12:44 +0900

    るーてぃんぐの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    post 'users/guest_sign_in', to: 'users#new_guest'
   end
-  
+
   root to: "tops#index"
   resources :shops do
     resources :reviews
