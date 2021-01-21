1b97ee3dfa27097853c3fe32d8352469e12ddff3
jus37 <jus_37@outlook.jp>
2021-01-22 05:17:19 +0900

    @shop変数の作成

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,6 +1,7 @@
 class HistoriesController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @reviews = Review.where(user_id: @user.id)
+    user = User.find(params[:user_id])
+    reviews = Review.where(user_id: user.id)
+    @shops = Shop.where(id: reviews.map{|review| review.shop_id})
   end
 end
